774c782e9b234f24572f9a8d7830bbb8d3dc5737
Sharon Moegi <sharamoegi@gmail.com>
2026-04-14 16:28:32 +0300

    Fetch posts from API

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+//Fetch Data from API
+async function fetchPosts() {
+    try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const posts = await res.json();
+        displayPosts(posts);
+    } catch (error) {
+        console.error("Error fetching posts:", error);
+    }
+    
+}
